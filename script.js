e14a1b8dfef441aabfe2a6014e1f30f65afcd065
sukhdeepsohal05 <sukhdeepsohal05@gmail.com>
2022-04-20 13:58:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const details = document.querySelectorAll('details');
+
+details.forEach(targetDetail =>{
+    targetDetail.addEventListener('click', ()=>{
+        details.forEach(detail=>{
+            if (detail !== targetDetail) {
+                detail.removeAttribute("open");
+            }
+        })
+    });
+});
